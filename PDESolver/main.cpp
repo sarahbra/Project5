b69251add13b376bda018c22b49fc05da661bdba
sarahbra <sarah.rastad@gmail.com>
2016-12-07 19:20:02 +0100

    Hei,hei!

--- PDESolver/main.cpp
+++ PDESolver/main.cpp
@@ -10,24 +10,24 @@ using namespace arma;
 
 ofstream ofile;
 
-void GaussElim(double a, vec b, double b_value, double c, int n, vec u, vec v){
+vec GaussElim(double a, vec b, double b_value, double c, int n, vec u, vec v){
     //Forward Substitution
     double m;
     for (int k=2; k<=n; k++) {
         m = a/b(k-1);
         b(k) = b_value - m*c;
-        u(k) = u(k) - m*u(k-1);
+        v(k) -= m*v(k-1);
     }
 
     //Backward Substitution
-    v(n)= u(n)/b(n);
-    cout << "|||||| x[n]" << v(n) << endl;
+    u(n)= v(n)/b(n);
     for (int k= n-1; k>0; k--) {
-        v(k) = (1.0/b(k))*(u(k) - c*v(k+1));
+        u(k) = (1.0/b(k))*(v(k) - c*u(k+1));
     }
 
-    v(0) = 0;
-    v(n+1) = 0;
+    u(0) = 0;
+    u(n) = 0;
+    return u;
 }
 
 
@@ -50,13 +50,11 @@ void printtofile(int t, vec u, int n ){
 void forward_Euler(int n, int t_steps, double alpha, double dx) {
     vec u(n+1);
     vec unew(n+1);
-    vec b(n+1);
 
     u(0) = unew(0) = u(n) = unew(n) = 0.0;
 
     //making the vectors
     for (int k=1; k<n; k++){
-        b(k) = b_value;
         u(k) = func(dx, k);
         unew(k) = 0;
     }
@@ -64,7 +62,6 @@ void forward_Euler(int n, int t_steps, double alpha, double dx) {
     for (int t=1; t<t_steps; t++) {
         for (int i=1; i<n; i++) {
             unew(i) = alpha*u(i-1) + (1-2*alpha) * u(i) + alpha*u(i+1);
-
         }
         u = unew;
         printtofile(t, u, n);
@@ -77,15 +74,14 @@ void backwards_Euler(int n, int t_steps, double alpha, double dx) {
     b_value = 1 + 2*alpha;
 
     vec b(n+1);
-
     vec u(n+1);
     vec v(n+1);
-
 
     //making the vectors
     for (int k=0; k<=n; k++){
         b(k) = b_value;
     }
+
     for (int k=1; k<n; k++) {
         v(k) = u(k) = func(dx,k);
     }
@@ -93,13 +89,8 @@ void backwards_Euler(int n, int t_steps, double alpha, double dx) {
     u(n) = v(n) = u(0) = v(0) = 0.0;
 
     for (int t=1; t<=t_steps; t++) {
-        GaussElim(a_value, b, b_value, c_value, n, v, u);
-        u(0) = 0.0;
-        u(n) = 0.0;
-        for (int i=0; i <= n; i++) {
-            v(i) = u(i);
-        }
-        printtofile(t,u,n);
+        v = GaussElim(a_value, b, b_value, c_value, n, u, v);
+        printtofile(t,v,n);
     }
 }
 
@@ -127,9 +118,8 @@ void crank_Nicholson(int n, int t_steps, double alpha, double dx) {
             v(i) = alpha*u(i-1) + (2-2*alpha)*u(i) + alpha*u(i+1);
         }
         v(0) = v(n) = 0;
-        GaussElim(a_value,b, b_value,c_value,n+1,v,u);
-        u(0) = u(n) = 0.0;
-        printtofile(t,u,n);
+        u = GaussElim(a_value,b, b_value,c_value,n,u,v);
+        printtofile(t,v,n);
     }
 }
 
@@ -146,15 +136,16 @@ int main(){
     outfilename = "test.txt";
     n = 10;
     dx = 0.1;
-    dt = dx*dx*0.5;
+    dt = dx*dx*0.25;
     t_steps = n*n;
     alpha = dt/(dx*dx);
     ofile.open(outfilename);
     clock_t start, finish;
     start = clock();
 
-    //forward_Euler(n, t_steps, alpha, dx);
-    backwards_Euler(n,t_steps,alpha,dx);
+    //forward_Euler(n,t_steps,alpha,dx);
+    //backwards_Euler(n,t_steps,alpha,dx);
+    crank_Nicholson(n,t_steps,alpha,dx);
 
     finish =clock();
     double t = ((finish-start));
